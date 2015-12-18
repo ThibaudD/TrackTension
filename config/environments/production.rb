609f9876ad62341ac9793b769a8a234468f318d8
Thib <thibaud.de@gmail.com>
2015-12-18 01:42:51 +0100

    add sendgrid conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'tracktension.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
